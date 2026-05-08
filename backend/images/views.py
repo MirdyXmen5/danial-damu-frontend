7724d6ba278e0cc930f5b0a82c06258cfbe69e1a
Mirdy <128206125+MirdyXmen5@users.noreply.github.com>
2026-05-08 21:45:52 +0500

    Update views.py

--- backend/images/views.py
+++ backend/images/views.py
@@ -1,13 +1,14 @@
 from rest_framework import viewsets, permissions
 from rest_framework.parsers import MultiPartParser, FormParser
-from .models import ProductImage
-from .serializers import ProductImageSerializer
+from .models import ProductImage, Shop
+from .serializers import ProductImageSerializer, ShopSerializer
+
 
 class ProductImageViewSet(viewsets.ModelViewSet):
     queryset = ProductImage.objects.all()
     serializer_class = ProductImageSerializer
     parser_classes = (MultiPartParser, FormParser)
-    permission_classes = [permissions.IsAuthenticated]
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
 
     def get_queryset(self):  # ← Эта остается!
         queryset = ProductImage.objects.all()
@@ -15,3 +16,9 @@ class ProductImageViewSet(viewsets.ModelViewSet):
         if category is not None:
             queryset = queryset.filter(category=category)
         return queryset
+
+
+class ShopViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = Shop.objects.all()
+    serializer_class = ShopSerializer
+    permission_classes = [permissions.AllowAny]
